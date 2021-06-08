faa8dbbbda738709266d4b517a97ce389d0360ec
linyc74 <yclin.python@gmail.com>
2021-06-08 03:17:42 -0400

    version 1.0.0

---
+++ __main__.py
@@ -0,0 +1,80 @@
+import argparse
+import covid_variant
+
+
+__version__ = '1.0.0'
+
+
+class EntryPoint:
+
+    parser: argparse.ArgumentParser
+
+    def main(self):
+        self.set_parser()
+        self.add_required_arguments()
+        self.add_optional_arguments()
+        args = self.parser.parse_args()
+        covid_variant.main(
+            fq1=args.fq1,
+            fq2=args.fq2,
+            outdir=args.outdir,
+            threads=args.threads,
+            debug=args.debug)
+
+    def set_parser(self):
+        prog = 'python covid_variant'
+
+        description = f'Covid Variant (v{__version__}) by Yu-Cheng Lin (yclin.python@gmail.com)'
+
+        dependencies = '\n  '.join([
+            'python (>=3.6)',
+            'pandas (0.25.1)',
+            'ngslite (1.1.2)',
+            'trim_galore (0.6.6)',
+            'bowtie2 (2.4.1)',
+            'samtools (1.10)',
+            'bcftools (1.9)',
+        ])
+
+        epilog = f'dependency:\n  {dependencies}'
+
+        self.parser = argparse.ArgumentParser(
+            prog=prog,
+            description=description,
+            epilog=epilog,
+            add_help=False,
+            formatter_class=argparse.RawTextHelpFormatter)
+
+    def add_required_arguments(self):
+        group = self.parser.add_argument_group('required arguments')
+
+        group.add_argument(
+            '-1', '--fq1', type=str, required=True,
+            help='path to read 1 fastq file')
+
+        group.add_argument(
+            '-2', '--fq2', type=str, required=True,
+            help='path to read 2 fastq file')
+
+    def add_optional_arguments(self):
+        group = self.parser.add_argument_group('optional arguments')
+        default = '(default: %(default)s)'
+
+        group.add_argument(
+            '-o', '--outdir', type=str, required=False, default='covid_variant_outdir',
+            help=f'path to the output directory {default}')
+
+        group.add_argument(
+            '-t', '--threads', type=int, required=False, default=4,
+            help=f'number of CPU threads {default}')
+
+        group.add_argument(
+            '-d', '--debug', action='store_true', help='debug mode')
+
+        group.add_argument(
+            '-h', '--help', action='help',
+            help='show this help message and exit')
+
+
+if __name__ == '__main__':
+    EntryPoint().main()
---
+++ covid_variant/__init__.py
@@ -0,0 +1,91 @@
+from os import makedirs
+from shutil import rmtree
+from os.path import exists, dirname
+from .template import Settings
+from .covid_variant import CovidVariant
+
+
+def get_temp_path(prefix: str = 'temp', suffix: str = '') -> str:
+    i = 0
+    while True:
+        path = f'{prefix}_{i:06d}{suffix}'
+        if not exists(path):
+            break
+        i += 1
+    return path
+
+
+class Main:
+
+    fq1: str
+    fq2: str
+    outdir: str
+    threads: int
+    debug: bool
+
+    settings: Settings
+    gbk: str
+    covid_strain_csv: str
+
+    def main(
+            self,
+            fq1: str,
+            fq2: str,
+            outdir: str,
+            threads: int,
+            debug: bool):
+
+        self.fq1 = fq1
+        self.fq2 = fq2
+        self.outdir = outdir
+        self.threads = threads
+        self.debug = debug
+
+        self.set_settings()
+        self.makedirs()
+        self.set_reference_paths()
+        self.execute()
+        self.clean_up()
+
+    def set_settings(self):
+        self.settings = Settings(
+            workdir=get_temp_path(prefix='workdir'),
+            outdir=self.outdir,
+            threads=self.threads,
+            debug=self.debug,
+            mock=False)
+
+    def makedirs(self):
+        for d in [self.settings.workdir, self.settings.outdir]:
+            makedirs(d, exist_ok=True)
+
+    def set_reference_paths(self):
+        ref_dir = f'{dirname(dirname(__file__))}/reference'
+        self.gbk = f'{ref_dir}/NC_045512.2.gb'
+        self.covid_strain_csv = f'{ref_dir}/covid_strains.csv'
+
+    def execute(self):
+        CovidVariant(self.settings).main(
+            gbk=self.gbk,
+            fq1=self.fq1,
+            fq2=self.fq2,
+            covid_strain_csv=self.covid_strain_csv)
+
+    def clean_up(self):
+        if not self.debug:
+            rmtree(self.settings.workdir)
+
+
+def main(
+        fq1: str,
+        fq2: str,
+        outdir: str,
+        threads: int,
+        debug: bool):
+
+    Main().main(
+        fq1=fq1,
+        fq2=fq2,
+        outdir=outdir,
+        threads=threads,
+        debug=debug)
