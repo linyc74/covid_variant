598287fa7d5bac0b899ce64549f25a44997abf42
linyc74 <yclin.python@gmail.com>
2021-06-29 13:23:58 -0400

    refactor __main__.py

--- __main__.py
+++ __main__.py
@@ -2,7 +2,93 @@ import argparse
 import covid_variant
 
 
-__version__ = '1.0.8'
+__version__ = '1.0.9-beta'
+
+
+PROG = 'python covid_variant'
+DESCRIPTION = f'Covid Variant (version {__version__}) by Yu-Cheng Lin (yclin.python@gmail.com)'
+DEPENDENCIES = [
+    'python (>=3.8)',
+    'fastqc (0.11.9)',
+    'cutadapt (3.3)',
+    'trim_galore (0.6.6)',
+    'bowtie2 (2.4.1)',
+    'samtools (1.9)',
+    'bcftools (1.9)',
+    'pandas (1.2.4)',
+    'ngslite (1.2.1)',
+    'biopython (1.79)',
+]
+REQUIRED = [
+    {
+        'keys': ['-1', '--fq1'],
+        'properties': {
+            'type': str,
+            'required': True,
+            'help': 'path to read 1 fastq file',
+        }
+    },
+]
+OPTIONAL = [
+    {
+        'keys': ['-2', '--fq2'],
+        'properties': {
+            'type': str,
+            'required': False,
+            'default': 'None',
+            'help': 'path to read 2 fastq file (default: %(default)s)',
+        }
+    },
+    {
+        'keys': ['-o', '--outdir'],
+        'properties': {
+            'type': str,
+            'required': False,
+            'default': 'covid_variant_outdir',
+            'help': 'path to the output directory (default: %(default)s)',
+        }
+    },
+    {
+        'keys': ['-m', '--tolerate-missing'],
+        'properties': {
+            'type': float,
+            'required': False,
+            'default': 0.1,
+            'help': 'fraction of missing mutations to be tolerated when matching to known variants (default: %(default)s)',
+        }
+    },
+    {
+        'keys': ['-t', '--threads'],
+        'properties': {
+            'type': int,
+            'required': False,
+            'default': 4,
+            'help': 'number of CPU threads (default: %(default)s)',
+        }
+    },
+    {
+        'keys': ['-d', '--debug'],
+        'properties': {
+            'action': 'store_true',
+            'help': 'debug mode',
+        }
+    },
+    {
+        'keys': ['-h', '--help'],
+        'properties': {
+            'action': 'help',
+            'help': 'show this help message',
+        }
+    },
+    {
+        'keys': ['-v', '--version'],
+        'properties': {
+            'action': 'version',
+            'version': __version__,
+            'help': 'show version',
+        }
+    },
+]
 
 
 class EntryPoint:
@@ -16,69 +102,22 @@ class EntryPoint:
         self.run()
 
     def set_parser(self):
-        prog = 'python covid_variant'
-
-        description = f'Covid Variant (version {__version__}) by Yu-Cheng Lin (yclin.python@gmail.com)'
-
-        dependencies = '\n  '.join([
-            'python (>=3.8)',
-            'fastqc (0.11.9)',
-            'cutadapt (3.3)',
-            'trim_galore (0.6.6)',
-            'bowtie2 (2.4.1)',
-            'samtools (1.9)',
-            'bcftools (1.9)',
-            'pandas (1.2.4)',
-            'ngslite (1.2.1)',
-            'biopython (1.79)',
-        ])
-
-        epilog = f'dependency:\n  {dependencies}'
-
         self.parser = argparse.ArgumentParser(
-            prog=prog,
-            description=description,
-            epilog=epilog,
+            prog=PROG,
+            description=DESCRIPTION,
+            epilog='\n  '.join(['dependency:'] + DEPENDENCIES),
             add_help=False,
             formatter_class=argparse.RawTextHelpFormatter)
 
     def add_required_arguments(self):
         group = self.parser.add_argument_group('required arguments')
-
-        group.add_argument(
-            '-1', '--fq1', type=str, required=True,
-            help='path to read 1 fastq file')
+        for item in REQUIRED:
+            group.add_argument(*item['keys'], **item['properties'])
 
     def add_optional_arguments(self):
         group = self.parser.add_argument_group('optional arguments')
-        default = '(default: %(default)s)'
-
-        group.add_argument(
-            '-2', '--fq2', type=str, required=False, default='None',
-            help='path to read 2 fastq file')
-
-        group.add_argument(
-            '-o', '--outdir', type=str, required=False, default='covid_variant_outdir',
-            help=f'path to the output directory {default}')
-
-        group.add_argument(
-            '-m', '--tolerate-missing', type=float, required=False, default=0.1,
-            help=f'fraction of missing mutations to be tolerated when matching to known variants {default}')
-
-        group.add_argument(
-            '-t', '--threads', type=int, required=False, default=4,
-            help=f'number of CPU threads {default}')
-
-        group.add_argument(
-            '-d', '--debug', action='store_true', help='debug mode')
-
-        group.add_argument(
-            '-h', '--help', action='help',
-            help='show this help message')
-
-        group.add_argument(
-            '-v', '--version', action='version', version=__version__,
-            help='show version')
+        for item in OPTIONAL:
+            group.add_argument(*item['keys'], **item['properties'])
 
     def run(self):
         args = self.parser.parse_args()
